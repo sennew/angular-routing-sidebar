0775bbc173cad9efa639cd289fd1151008ed6ade
Senne Wens <sennewens@hotmail.com>
2018-09-11 20:47:01 +0200

    sidenavigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Product } from './products/product.component';
+import { Users } from './users/user.component';
+
+const appRoutes: Routes = [
+  { path: 'products', component: Product },
+  { path: 'users', component: Users}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Product,
+    Users
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
+
 export class AppModule { }
